08c4cc3fb76bb0859421dd826a5825904d5e8534
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-02 09:28:55 +0900

    feat:add xml logging when parsing failed

--- main.ts
+++ main.ts
@@ -2,41 +2,43 @@ import { parseFeed } from "https://deno.land/x/rss@0.5.3/mod.ts";
 
 const link = Deno.env.get("RSS_LINK") ?? "";
 const response = await fetch(link);
-
 const xml = await response.text();
-const { entries } = await parseFeed(xml);
 
-<<<<<<< HEAD
-console.log(entries);
-=======
 const lastPublishedText = (await Deno.readTextFile("./last_published.txt")).trim();
-const lastPublished = new Date(lastPublishedText);
+try {
+  const { entries } = await parseFeed(xml);
 
-const newEntries = entries.filter((it) => it.published !== undefined && it.published > lastPublished);
-console.log(newEntries);
-const embeds: Embed[] = newEntries.map((it) => ({
-  title: it.title?.value,
-  description: textClipper(
-    unescape(it.description?.value ?? '')
-      .replace(/<[^>]*>/g, "")
-    , 100,
-    { html: false, indicator: '...' },
-  ),
-  url: it.links[0].href,
-}))
+  const lastPublishedText = (await Deno.readTextFile("./last_published.txt")).trim();
+  const lastPublished = new Date(lastPublishedText);
 
-if (embeds.length > 0) {
-  const bot = discordeno.createBot({
-    token,
-    botId,
-  })
-  discordeno.sendMessage(
-    bot,
-    channelId,
-    {
-      embeds,
-    }
-  )
+  const newEntries = entries.filter((it) => it.published !== undefined && it.published > lastPublished);
+  console.log(newEntries);
+  const embeds: Embed[] = newEntries.map((it) => ({
+    title: it.title?.value,
+    description: textClipper(
+      unescape(it.description?.value ?? '')
+        .replace(/<[^>]*>/g, "")
+      , 100,
+      { html: false, indicator: '...' },
+    ),
+    url: it.links[0].href,
+  }))
+
+  if (embeds.length > 0) {
+    const bot = discordeno.createBot({
+      token,
+      botId,
+    })
+    discordeno.sendMessage(
+      bot,
+      channelId,
+      {
+        embeds,
+      }
+    )
+  }
+} catch (e) {
+  console.error(xml);
+  console.error(e);
+  Deno.exit(1);
 }
-
->>>>>>> 26cd4bd (test:add request log)
