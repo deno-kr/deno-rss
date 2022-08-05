34d939073bb415bd44be5487108a838d7fcdf208
Changseo Jang <changseo.jang@mindslab.ai>
2022-08-05 09:42:32 +0900

    fix:merge error

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 import { parseFeed } from "https://deno.land/x/rss@0.5.3/mod.ts";
+import * as discordeno from "https://deno.land/x/discordeno@13.0.0-rc45/mod.ts";
+import type { Embed } from "https://deno.land/x/discordeno@13.0.0-rc45/transformers/embed.ts";
+import { unescape } from "https://deno.land/x/html_escape@v1.1.5/unescape.ts";
+import textClipper from "https://deno.land/x/text_clipper@2.1.0/mod.ts";
+
+const token = Deno.env.get("BOT_TOKEN") ?? "";
+const botId = BigInt(Deno.env.get("BOT_ID") ?? "");
+const channelId = BigInt(Deno.env.get("CHANNEL_ID") ?? "");
 
 const link = Deno.env.get("RSS_LINK") ?? "";
 const response = await fetch(link);
 const xml = await response.text();
 
-const lastPublishedText = (await Deno.readTextFile("./last_published.txt")).trim();
 try {
   const { entries } = await parseFeed(xml);
 
